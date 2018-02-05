a6098c5d73467573c05d21fd27c73c5e28cb5b2e
MartinBG <MartinBG@abv.bg>
2018-02-05 07:16:17 +0200

    Another solution for "15. Show All Games with Duration and Part of the Day"

--- 04. Built-in Functions/Built-in Functions Exercises.sql
+++ 04. Built-in Functions/Built-in Functions Exercises.sql
@@ -220,6 +220,23 @@ FROM
     `games`
 ORDER BY `name`;
 
+-- Another solution
+SELECT 
+    `name` AS 'game',
+    CASE
+        WHEN HOUR(`start`) < 12 THEN 'Morning'
+        WHEN HOUR(`start`) < 18 THEN 'Afternoon'
+        ELSE 'Evening'
+    END AS 'Part of the Day',
+    CASE
+        WHEN `duration` < 4 THEN 'Extra Short'
+        WHEN `duration` < 7 THEN 'Short'
+        WHEN `duration` < 11 THEN 'Long'
+        ELSE 'Extra Long'
+    END AS 'Duration'
+FROM
+    `games`
+ORDER BY `name`;
 
 -- Part IV – Date Functions Queries
 

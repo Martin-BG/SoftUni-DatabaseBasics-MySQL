14fc5e9e063060ea1b72233a774f38cf3033f8d7
MartinBG <MartinBG@abv.bg>
2018-02-16 19:25:07 +0200

    Formatting

--- 02. Data Definition and Datatypes/Data Definition and Datatypes Exercise.sql
+++ 02. Data Definition and Datatypes/Data Definition and Datatypes Exercise.sql
@@ -47,10 +47,10 @@ ALTER TABLE `minions`
 -- 04. Insert Records in Both Tables
 -- Populate both tables with sample records given in the table below.
 -- minions								towns
--- id	name		age	town_id		id	name
--- 1	Kevin		22		1		1	Sofia
--- 2	Bob			15		3		2	Plovdiv
--- 3	Steward		NULL 	2		3	Varna
+-- id	name		age		town_id		id	name
+-- 1	Kevin		22		1			1	Sofia
+-- 2	Bob			15		3			2	Plovdiv
+-- 3	Steward		NULL 	2			3	Varna
 -- Use only insert SQL queries.
 
 INSERT INTO `towns`(`id`, `name`)
@@ -122,12 +122,12 @@ INSERT INTO `people`(`name`, `gender`, `birthdate`)
 -- Submit your CREATE and INSERT statements
 
 CREATE TABLE `users` (
-    `id` BIGINT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `username` VARCHAR(30) UNIQUE NOT NULL,
-    `password` VARCHAR(26) NOT NULL,
-    `profile_picture` BLOB,
-    `last_login_time` TIMESTAMP,
-    `is_deleted` BOOLEAN
+	`id` BIGINT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
+	`username` VARCHAR(30) UNIQUE NOT NULL,
+	`password` VARCHAR(26) NOT NULL,
+	`profile_picture` BLOB,
+	`last_login_time` TIMESTAMP,
+	`is_deleted` BOOLEAN
 );
 
 INSERT INTO `users`
@@ -148,7 +148,7 @@ VALUES
 
 ALTER TABLE `users` 
 	DROP PRIMARY KEY,
-    ADD CONSTRAINT `pk_users` PRIMARY KEY (`id`, `username`);
+	ADD CONSTRAINT `pk_users` PRIMARY KEY (`id`, `username`);
 
 
 -- 10. Set Default Value of a Field
@@ -157,7 +157,7 @@ ALTER TABLE `users`
 
 ALTER TABLE `users` 
 	MODIFY COLUMN `last_login_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP; -- now()
-    
+
 ALTER TABLE `users`
 	CHANGE COLUMN `last_login_time` `last_login_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
 
@@ -170,9 +170,9 @@ ALTER TABLE `users`
 
 ALTER TABLE `users`
 	DROP PRIMARY KEY,
-    ADD CONSTRAINT PRIMARY KEY (`id`),
-    ADD CONSTRAINT UNIQUE (`username`);
-    
+	ADD CONSTRAINT PRIMARY KEY (`id`),
+	ADD CONSTRAINT UNIQUE (`username`);
+
 
 -- 12. Movies Database
 -- Using SQL queries create Movies database with the following entities:
@@ -193,32 +193,32 @@ USE `movies`;
 
 CREATE TABLE `directors` (
 	`id` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `director_name` VARCHAR(30) NOT NULL,
-    `notes` TEXT
+	`director_name` VARCHAR(30) NOT NULL,
+	`notes` TEXT
 );
 
 CREATE TABLE `genres` (
 	`id` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `genre_name` VARCHAR(30) NOT NULL,
-    `notes` TEXT
+	`genre_name` VARCHAR(30) NOT NULL,
+	`notes` TEXT
 );
 
 CREATE TABLE `categories` (
 	`id` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `category_name` VARCHAR(30) NOT NULL,
-    `notes` TEXT
+	`category_name` VARCHAR(30) NOT NULL,
+	`notes` TEXT
 );
 
 CREATE TABLE `movies` (
 	`id` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `title` VARCHAR(30) NOT NULL,
-    `director_id` INT UNSIGNED NOT NULL,
-    `copyright_year` YEAR NOT NULL,
-    `length` TIME NOT NULL,
-    `genre_id` INT UNSIGNED NOT NULL,
-    `category_id` INT UNSIGNED NOT NULL,
-    `rating` DOUBLE NOT NULL DEFAULT 0,
-    `notes` TEXT
+	`title` VARCHAR(30) NOT NULL,
+	`director_id` INT UNSIGNED NOT NULL,
+	`copyright_year` YEAR NOT NULL,
+	`length` TIME NOT NULL,
+	`genre_id` INT UNSIGNED NOT NULL,
+	`category_id` INT UNSIGNED NOT NULL,
+	`rating` DOUBLE NOT NULL DEFAULT 0,
+	`notes` TEXT
 );
 
 INSERT INTO `movies`
@@ -234,10 +234,10 @@ INSERT INTO `directors`
 	(`id`, `director_name`, `notes`)
 VALUES
 	(1,'dasdasd','fasdfasdfasdfa'),
-    (2,'dasdasd','fasdfasdfasdfa'),
-    (3,'dasdasd','fasdfasdfasdfa'),
-    (4,'dasdasd','fasdfasdfasdfa'),
-    (5,'dasdasd','fasdfasdfasdfa');
+	(2,'dasdasd','fasdfasdfasdfa'),
+	(3,'dasdasd','fasdfasdfasdfa'),
+	(4,'dasdasd','fasdfasdfasdfa'),
+	(5,'dasdasd','fasdfasdfasdfa');
 
 INSERT INTO `categories`
 	(`id`, `category_name`)
@@ -252,11 +252,11 @@ INSERT INTO `genres`
 	( `id`, `genre_name`, `notes`)
 VALUES
 	(2,'dasdad','kaman'),
-    (1,'dasdad','kaman'),
-    (3,'dasdad','kaman'),
-    (4,'dasdad','kaman'),
-    (5,'dasdad','kaman');
-    
+	(1,'dasdad','kaman'),
+	(3,'dasdad','kaman'),
+	(4,'dasdad','kaman'),
+	(5,'dasdad','kaman');
+
 
 -- 13. Car Rental Database
 -- Using SQL queries create car_rental database with the following entities:
@@ -280,11 +280,11 @@ USE `car_rental`;
 
 CREATE TABLE `categories` (
 	`id` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `category` VARCHAR(30) NOT NULL,
-    `daily_rate` DOUBLE NOT NULL,
-    `weekly_rate` DOUBLE NOT NULL,
-    `monthly_rate` DOUBLE NOT NULL,
-    `weekend_rate` DOUBLE NOT NULL
+	`category` VARCHAR(30) NOT NULL,
+	`daily_rate` DOUBLE NOT NULL,
+	`weekly_rate` DOUBLE NOT NULL,
+	`monthly_rate` DOUBLE NOT NULL,
+	`weekend_rate` DOUBLE NOT NULL
 );
 
 INSERT INTO `categories`
@@ -293,18 +293,18 @@ INSERT INTO `categories`
 		('Category 1', 1.1, 2.1, 3.1, 4.1),
 		('Category 2', 1.2, 2.2, 3.2, 4.2),
 		('Category 3', 1.3, 2.3, 3.3, 4.3);
-        
+
 CREATE TABLE `cars` (
 	`id` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `plate_number` VARCHAR(20) NOT NULL UNIQUE,
-    `make` VARCHAR(20) NOT NULL,
-    `model` VARCHAR(20) NOT NULL,
-    `car_year` YEAR NOT NULL,
-    `category_id` INT UNSIGNED NOT NULL,
-    `doors` TINYINT UNSIGNED NOT NULL,
-    `picture` BLOB,
-    `car_condition` VARCHAR(20),
-    `available` BOOLEAN NOT NULL DEFAULT TRUE
+	`plate_number` VARCHAR(20) NOT NULL UNIQUE,
+	`make` VARCHAR(20) NOT NULL,
+	`model` VARCHAR(20) NOT NULL,
+	`car_year` YEAR NOT NULL,
+	`category_id` INT UNSIGNED NOT NULL,
+	`doors` TINYINT UNSIGNED NOT NULL,
+	`picture` BLOB,
+	`car_condition` VARCHAR(20),
+	`available` BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 INSERT INTO `cars`
@@ -316,10 +316,10 @@ INSERT INTO `cars`
 
 CREATE TABLE `employees` (
 	`id` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `first_name` VARCHAR(30) NOT NULL,
-    `last_name` VARCHAR(30) NOT NULL,
-    `title` VARCHAR(30) NOT NULL,
-    `notes` VARCHAR(128)
+	`first_name` VARCHAR(30) NOT NULL,
+	`last_name` VARCHAR(30) NOT NULL,
+	`title` VARCHAR(30) NOT NULL,
+	`notes` VARCHAR(128)
 );
 
 INSERT INTO `employees`
@@ -331,12 +331,12 @@ INSERT INTO `employees`
 
 CREATE TABLE `customers` (
 	`id` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `driver_licence_number` VARCHAR(30) NOT NULL,
-    `full_name` VARCHAR(60) NOT NULL,
-    `address` VARCHAR(50) NOT NULL,
-    `city` VARCHAR(20) NOT NULL,
-    `zip_code` INT(4) NOT NULL,
-    `notes` VARCHAR(128)
+	`driver_licence_number` VARCHAR(30) NOT NULL,
+	`full_name` VARCHAR(60) NOT NULL,
+	`address` VARCHAR(50) NOT NULL,
+	`city` VARCHAR(20) NOT NULL,
+	`zip_code` INT(4) NOT NULL,
+	`notes` VARCHAR(128)
 );
 
 INSERT INTO `customers`
@@ -348,21 +348,21 @@ INSERT INTO `customers`
 
 CREATE TABLE `rental_orders` (
 	`id` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `employee_id` INT UNSIGNED NOT NULL,
-    `customer_id` INT UNSIGNED NOT NULL,
-    `car_id` INT UNSIGNED NOT NULL,
-    `car_condition` VARCHAR(20),
-    `tank_level` DOUBLE,
-    `kilometrage_start` DOUBLE,
-    `kilometrage_end` DOUBLE,
-    `total_kilometrage` DOUBLE,
-    `start_date` DATE,
-    `end_date` DATE,
-    `total_days` INT UNSIGNED,
-    `rate_applied` DOUBLE,
-    `tax_rate` DOUBLE,
-    `order_status` VARCHAR(30),
-    `notes` VARCHAR(128)
+	`employee_id` INT UNSIGNED NOT NULL,
+	`customer_id` INT UNSIGNED NOT NULL,
+	`car_id` INT UNSIGNED NOT NULL,
+	`car_condition` VARCHAR(20),
+	`tank_level` DOUBLE,
+	`kilometrage_start` DOUBLE,
+	`kilometrage_end` DOUBLE,
+	`total_kilometrage` DOUBLE,
+	`start_date` DATE,
+	`end_date` DATE,
+	`total_days` INT UNSIGNED,
+	`rate_applied` DOUBLE,
+	`tax_rate` DOUBLE,
+	`order_status` VARCHAR(30),
+	`notes` VARCHAR(128)
 );
 
 INSERT INTO `rental_orders`
@@ -399,10 +399,10 @@ USE `hotel`;
 
 CREATE TABLE `employees` (
 	`id` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `first_name` VARCHAR(30) NOT NULL,
-    `last_name` VARCHAR(30) NOT NULL,
-    `title` VARCHAR(30) NOT NULL,
-    `notes` VARCHAR(128)
+	`first_name` VARCHAR(30) NOT NULL,
+	`last_name` VARCHAR(30) NOT NULL,
+	`title` VARCHAR(30) NOT NULL,
+	`notes` VARCHAR(128)
 );
 
 INSERT INTO `employees`
@@ -414,12 +414,12 @@ INSERT INTO `employees`
 
 CREATE TABLE `customers` (
 	`account_number` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `first_name` VARCHAR(30) NOT NULL,
-    `last_name` VARCHAR(30) NOT NULL,
-    `phone_number` VARCHAR(20) NOT NULL,
-    `emergency_name` VARCHAR(50),
-    `emergency_number` VARCHAR(20),
-    `notes` VARCHAR(128)
+	`first_name` VARCHAR(30) NOT NULL,
+	`last_name` VARCHAR(30) NOT NULL,
+	`phone_number` VARCHAR(20) NOT NULL,
+	`emergency_name` VARCHAR(50),
+	`emergency_number` VARCHAR(20),
+	`notes` VARCHAR(128)
 );
 
 INSERT INTO `customers`
@@ -430,8 +430,8 @@ INSERT INTO `customers`
 		('Bai', 'Ivan', '007');
 
 CREATE TABLE `room_status` (
-    `room_status` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `notes` VARCHAR(128)
+	`room_status` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
+	`notes` VARCHAR(128)
 );
 
 INSERT INTO `room_status` 
@@ -440,10 +440,10 @@ INSERT INTO `room_status`
 		('Free'),
 		('For clean'),
 		('Occupied');
-        
+
 CREATE TABLE `room_types` (
-    `room_type` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `notes` VARCHAR(128)
+	`room_type` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
+	`notes` VARCHAR(128)
 );
 
 INSERT INTO `room_types` 
@@ -455,8 +455,8 @@ INSERT INTO `room_types`
 
 
 CREATE TABLE `bed_types` (
-    `bed_type` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `notes` VARCHAR(128)
+	`bed_type` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
+	`notes` VARCHAR(128)
 );
 
 INSERT INTO `bed_types` 
@@ -465,14 +465,14 @@ INSERT INTO `bed_types`
 		('Single'),
 		('Double'),
 		('Water-filled');
-        
+
 CREATE TABLE `rooms` (
 	`room_number` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `room_type` INT UNSIGNED NOT NULL,
-    `bed_type` INT UNSIGNED NOT NULL,
-    `rate` DOUBLE DEFAULT 0,
-    `room_status` INT UNSIGNED NOT NULL,
-    `notes` VARCHAR(128)
+	`room_type` INT UNSIGNED NOT NULL,
+	`bed_type` INT UNSIGNED NOT NULL,
+	`rate` DOUBLE DEFAULT 0,
+	`room_status` INT UNSIGNED NOT NULL,
+	`notes` VARCHAR(128)
 );
 
 INSERT INTO `rooms` 
@@ -484,17 +484,17 @@ INSERT INTO `rooms`
 
 CREATE TABLE `payments` (
 	`id` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `employee_id` INT UNSIGNED NOT NULL,
-    `payment_date` DATE NOT NULL,
-    `account_number` INT UNSIGNED NOT NULL,
-    `first_date_occupied` DATE,
-    `last_date_occupied` DATE,
-    `total_days` INT UNSIGNED,
-    `amount_charged` DOUBLE,
-    `tax_rate` DOUBLE,
-    `tax_amount` DOUBLE,
-    `payment_total` DOUBLE,
-    `notes` VARCHAR(128)
+	`employee_id` INT UNSIGNED NOT NULL,
+	`payment_date` DATE NOT NULL,
+	`account_number` INT UNSIGNED NOT NULL,
+	`first_date_occupied` DATE,
+	`last_date_occupied` DATE,
+	`total_days` INT UNSIGNED,
+	`amount_charged` DOUBLE,
+	`tax_rate` DOUBLE,
+	`tax_amount` DOUBLE,
+	`payment_total` DOUBLE,
+	`notes` VARCHAR(128)
 );
 
 INSERT INTO `payments` 
@@ -507,13 +507,13 @@ INSERT INTO `payments`
 
 CREATE TABLE `occupancies` (
 	`id` INT UNSIGNED PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-    `employee_id` INT UNSIGNED NOT NULL,
-    `date_occupied` DATE NOT NULL,
-    `account_number` INT UNSIGNED NOT NULL,
-    `room_number` INT UNSIGNED NOT NULL,
-    `rate_applied` DOUBLE,
-    `phone_charge` DOUBLE,
-    `notes` VARCHAR(128)
+	`employee_id` INT UNSIGNED NOT NULL,
+	`date_occupied` DATE NOT NULL,
+	`account_number` INT UNSIGNED NOT NULL,
+	`room_number` INT UNSIGNED NOT NULL,
+	`rate_applied` DOUBLE,
+	`phone_charge` DOUBLE,
+	`notes` VARCHAR(128)
 );
 
 INSERT INTO `occupancies` 
@@ -542,37 +542,40 @@ USE `soft_uni`;
 
 CREATE TABLE `towns` (
 	`id` INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
-    `name` VARCHAR(30) NOT NULL, 
-    CONSTRAINT `pk_towns` PRIMARY KEY (`id`)
+	`name` VARCHAR(30) NOT NULL,
+	CONSTRAINT `pk_towns` PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `addresses` (
 	`id` INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
-    `address_text` VARCHAR(30) NOT NULL,
-    `town_id` INT UNSIGNED,
-    CONSTRAINT `pk_addresses` PRIMARY KEY (`id`),
-    CONSTRAINT `fk_addresses_towns` FOREIGN KEY (`town_id`) REFERENCES `towns`(`id`)
+	`address_text` VARCHAR(30) NOT NULL,
+	`town_id` INT UNSIGNED,
+	CONSTRAINT `pk_addresses` PRIMARY KEY (`id`),
+	CONSTRAINT `fk_addresses_towns` FOREIGN KEY (`town_id`)
+		REFERENCES `towns` (`id`)
 );
 
 CREATE TABLE `departments` (
 	`id` INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
-    `name` VARCHAR(30) NOT NULL,
-    CONSTRAINT `pk_departments` PRIMARY KEY (`id`)
+	`name` VARCHAR(30) NOT NULL,
+	CONSTRAINT `pk_departments` PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `employees` (
 	`id` INT UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
-    `first_name` VARCHAR(30) NOT NULL,
-    `middle_name` VARCHAR(30),
-    `last_name` VARCHAR(30) NOT NULL,
-    `job_title` VARCHAR(30) NOT NULL,
-    `department_id` INT UNSIGNED,
-    `hire_date` DATE,
-    `salary` DECIMAL(10, 2),
-    `address_id` INT UNSIGNED,
-    CONSTRAINT `pk_employees` PRIMARY KEY (`id`),
-    CONSTRAINT `fk_employees_departments` FOREIGN KEY (`department_id`) REFERENCES `departments`(`id`),
-    CONSTRAINT `fk_employees_addresses` FOREIGN KEY (`address_id`) REFERENCES `addresses`(`id`)
+	`first_name` VARCHAR(30) NOT NULL,
+	`middle_name` VARCHAR(30),
+	`last_name` VARCHAR(30) NOT NULL,
+	`job_title` VARCHAR(30) NOT NULL,
+	`department_id` INT UNSIGNED,
+	`hire_date` DATE,
+	`salary` DECIMAL(10 , 2 ),
+	`address_id` INT UNSIGNED,
+	CONSTRAINT `pk_employees` PRIMARY KEY (`id`),
+	CONSTRAINT `fk_employees_departments` FOREIGN KEY (`department_id`)
+		REFERENCES `departments` (`id`),
+	CONSTRAINT `fk_employees_addresses` FOREIGN KEY (`address_id`)
+		REFERENCES `addresses` (`id`)
 );
 
 
@@ -615,7 +618,7 @@ INSERT INTO `towns`
 		('Plovdiv'),
 		('Varna'),
 		('Burgas');
-        
+
 INSERT INTO `departments` 
 		(`name`) 
 	VALUES
@@ -624,7 +627,7 @@ INSERT INTO `departments`
 		('Marketing'),
 		('Software Development'),
 		('Quality Assurance');
-    
+
 INSERT INTO `employees`
 		(`first_name`, `middle_name`, `last_name`, `job_title`, `department_id`, `hire_date`, `salary`)
 	VALUES
@@ -668,19 +671,19 @@ SELECT * FROM `employees` ORDER BY `salary` DESC;
 SELECT `name` FROM `towns` ORDER BY `name`;
 SELECT `name` FROM `departments` ORDER BY `name`;
 SELECT 
-    `first_name`, `last_name`, `job_title`, `salary`
+	`first_name`, `last_name`, `job_title`, `salary`
 FROM
-    `employees`
+	`employees`
 ORDER BY `salary` DESC;
 
 
 -- 21. Increase Employees Salary
--- Use softuni database and increase the salary of all employees by 10%. 
+-- Use soft_uni database and increase the salary of all employees by 10%. 
 -- Select only salary column from the employees table. 
 
 UPDATE `employees` 
 SET 
-    `salary` = `salary` * 1.1;
+	`salary` = `salary` * 1.1;
 
 SELECT `salary` FROM `employees`;
 
@@ -691,7 +694,7 @@ SELECT `salary` FROM `employees`;
 
 UPDATE `payments` 
 SET 
-    `tax_rate` = `tax_rate` * 0.97;
+	`tax_rate` = `tax_rate` * 0.97;
 
 SELECT `tax_rate` FROM `payments`;
 
